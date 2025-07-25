a3d1707cbb052080b427612b233f236476cabcc8
Clutch Keys <keysclutch@gmail.com>
2025-07-25 19:41:28 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -27,6 +27,12 @@ const nextConfig: NextConfig = {
         hostname: 'firebasestorage.googleapis.com',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.freebiesupply.com',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
